9826fbddf2a2a4834a55e4df37b762c9433d2499
Nikkypwetti <nikkypwetti@gmail.com>
2026-01-23 19:23:04 +0100

    refactor: fix tflint issues and add mandatory tags

--- 03-infrastructure-as-code/code-labs/terraform-labs/terraform-ci-cd/main.tf
+++ 03-infrastructure-as-code/code-labs/terraform-labs/terraform-ci-cd/main.tf
@@ -1,7 +1,22 @@
+terraform {
+  required_version = ">= 1.5.0" # Use your current version or higher
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0" # Or whichever version you are using
+    }
+  }
+}
 provider "aws" {
-  region = "us-east-1"
+  region = var.aws_region
 }
 
 resource "aws_s3_bucket" "my_practice_bucket" {
-  bucket = "nikky-devops-practice-2026-x1" # Change this name!
+  bucket = var.bucket_name
+
+  tags = {
+    Environment = "Dev"
+    Owner       = "Nikky"
+  }
 }
