2cefa51e93d70168dcfb188b9d096fd98a686ecd
Nikkypwetti <nikkypwetti@gmail.com>
2026-01-23 19:38:55 +0100

    fix tfrunsec error

--- 03-infrastructure-as-code/code-labs/terraform-labs/terraform-ci-cd/main.tf
+++ 03-infrastructure-as-code/code-labs/terraform-labs/terraform-ci-cd/main.tf
@@ -12,11 +12,38 @@ provider "aws" {
   region = var.aws_region
 }
 
+# 1. The Bucket
 resource "aws_s3_bucket" "my_practice_bucket" {
   bucket = var.bucket_name
-
   tags = {
     Environment = "Dev"
     Owner       = "Nikky"
+  }
+}
+
+# 2. Block all public access 
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket                  = aws_s3_bucket.my_practice_bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+# 3. Enable Encryption 
+resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
+  bucket = aws_s3_bucket.my_practice_bucket.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+# 4. Enable Versioning 
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.my_practice_bucket.id
+  versioning_configuration {
+    status = "Enabled"
   }
 }
